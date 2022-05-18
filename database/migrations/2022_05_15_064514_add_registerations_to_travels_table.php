b1b899b44b2be7c147a07495f59ea48a92685c8f
zohrehhfz <hfz.zohreh@gmail.com>
2022-05-18 23:44:27 +0430

    first commit on master

---
+++ database/migrations/2022_05_15_064514_add_registerations_to_travels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->string('registerationstart')->after("traveltime");
+			$table->string('registerationend')->after("registerationstart");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->dropColumn('registerationstart');
+			$table->dropColumn('registerationend');
+        });
+    }
+};
