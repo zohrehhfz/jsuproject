8f59e7eeb870687d0a78292e8c81b71eae523305
zohrehhfz <hfz.zohreh@gmail.com>
2022-05-27 21:29:16 +0430

    write get travel in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
 	{
 		return $this->hasMany('App\Models\role');
 	}
+	
+	public function travels()
+	{
+		return $this->belongsToMany('App\Models\Travel');
+	}		
 }
