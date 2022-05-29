c3fc24223090c79a99fac841fa2a210dd9c39724
zohrehhfz <hfz.zohreh@gmail.com>
2022-05-29 13:11:30 +0430

    fetch travels with foreach in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,8 +56,13 @@
                     </div>
             </div> -->
 				
-
-
+					<div>
+					@foreach ($trvls as $t)
+					<p> {{$t->destination}} </p>
+					@endforeach
+					</div>
+					<br>
+					<br>
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
