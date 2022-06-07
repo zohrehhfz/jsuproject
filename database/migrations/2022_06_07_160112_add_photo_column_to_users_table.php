dac45408b2bcc22a0038d0ab1144ab66d6cfeaca
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-07 20:40:24 +0430

    add photo name and photo orginal name column to users table

---
+++ database/migrations/2022_06_07_160112_add_photo_column_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string("photoname")->after("email")->nullable();
+			$table->string("orginalphotoname")->after("photoname")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('photoname');
+			$table->dropColumn('orginalphotoname');
+        });
+    }
+};
