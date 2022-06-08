9e37acf340b8e490b690c17d7f0edbf7e752b958
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-08 21:20:21 +0430

    write construct function for middleware

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -13,6 +13,12 @@ class TravelController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+	 public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('admin')->only('index');
+    }
+	
     public function index()
     {
         $travels = Travel::orderBy('registerationend','DESC')->get();
