a333ceebfbb909e9e019aea0826b10962f774826
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-13 13:03:46 +0430

    write registeration info in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,40 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 bg-white border-b border-gray-200" dir=rtl>
+					
+					<p>نام :
+						@php
+						echo Auth::user()->name;
+						@endphp
+						</p>
+						<p> ایمیل :
+						@php
+						echo Auth::user()->email;
+						@endphp
+						</p>
+						<p> شماره تماس :
+						@php
+						echo Auth::user()->phone;
+						@endphp
+						</p>
+						<p> سال تولد :
+						@php
+						echo Auth::user()->birthdate;
+						@endphp
+						</p>
+						<p> حساب در تاریخ 
+						@php
+						echo Auth::user()->created_at;
+						@endphp
+						 ایجاد شده است </p>
                 </div>
+				
+				<div class="p-6 bg-white border-b border-gray-200" dir=rtl>
+					
+					
+                </div>
+				
             </div>
         </div>
     </div>
