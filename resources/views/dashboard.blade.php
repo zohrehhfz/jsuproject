dfa5991b499b14d57b2f2b549c630c2671a06106
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-16 00:30:21 +0430

    change title in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.my_layout')
-@section('title', 'dashboardd')
+@section('title', 'dashboard')
 @section('content')
 	<div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
