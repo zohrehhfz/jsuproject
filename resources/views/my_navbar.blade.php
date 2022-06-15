63d5bcc40aabff894f4449cd8e576ee9377086a8
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-16 00:27:20 +0430

    create my_layout and my_navbar and change dashboard layout

---
+++ resources/views/my_navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-sm bg-success navbar-dark sticky-top">
+  <div class="container-fluid"> 
+	<ul class="navbar-nav">
+		<li class="nav-item">
+			<a class="navbar-brand" href="{{ route('IndexTravel') }}" class="underline">  Travels  </a>
+		</li>
+		
+	</ul>
+  </div>
+</nav>
