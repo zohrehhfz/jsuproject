5d84693f330649c20a201eb82560e03ebe6e63de
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-22 19:48:50 +0430

    ADD active travel in show

--- resources/views/travels/show.blade.php
+++ resources/views/travels/show.blade.php
@@ -63,6 +63,7 @@
 								<p> پایان ثبت نام : {{$travel->registerationend}}</p>
 								<p> توضیحات سفر : {{$travel->description}}</p>
 								<p> تعداد افراد ثبت نام کرده در این سفر : {{$number}}</p>
+								
 								<br>
 								<br>
 					<?php
@@ -74,12 +75,12 @@
 							$currentyear = date("Y");
 							$currentmonth = date("m");
 							$currentday = date("d");
-						if(($currentyear <= $year) && ( $travel->cancel == 0))
+						if(($currentyear <= $year))
 						{	
 							if ((($currentmonth == $month) && ($currentday <= $day)) || ($currentmonth < $month))
 							{	
 						?>
-								@if(( $message == "0" ) || ( $message == "3" ))
+								@if((( $message == "0" ) || ( $message == "3" )) && ($travel->cancel == 0))
 								<button id="submitbutton"><a href="{{route('AddTravelForUser',[$travel])}}" style="color:white; text-decoration: none;">ثبت نام در سفر <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
   <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
@@ -105,9 +106,14 @@
 								<button id="submitbutton"><a href="{{ route('EditTravel',[$travel]) }}" style="color:white; text-decoration: none;"> تغییر اطلاعات سفر <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
 </svg></a></button>
-
 								@endif
+								@if((Auth::user()->roles->where("role","Admin")->count() == 1) && ($travel->cancel == 1))
+									<button id="submitbutton"><a href="{{route('CancleTravel',[$travel])}}" style="color:white; text-decoration: none;" >فعال کردن سفر <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+</svg></a></button>
 								
+								@endif
 								@endauth
 						<?php
 							}
