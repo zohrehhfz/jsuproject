49a3eff044ef5dca86bdd133577601ca7322acde
zohrehhfz <hfz.zohreh@gmail.com>
2022-06-25 16:53:00 +0430

    show admin pic in page

--- resources/views/panels/admin.blade.php
+++ resources/views/panels/admin.blade.php
@@ -2,105 +2,112 @@
 @section('title', 'پنل کاربری')
 @section('content')
 <nav class="navbar navbar-expand-sm navbar-dark sticky-top">
-  <div class="container-fluid"> 
-	
-			@if (Route::has('login'))
-				
-			     <a href="{{ route('IndexTravel') }}" class="navbar-brand hover:text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image-alt" viewBox="0 0 16 16" style="display:inline;">
-  <path d="M7 2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0zm4.225 4.053a.5.5 0 0 0-.577.093l-3.71 4.71-2.66-2.772a.5.5 0 0 0-.63.062L.002 13v2a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4.5l-4.777-3.947z"/>
-</svg> سفرها</a>
-				
-                    @auth
-					@if(Auth::user()->roles->where("role",'Admin')->count() == "1")
-						<a href="{{ route('CreateTravel') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16" style="display:inline;">
-  <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-</svg>  افزودن سفر </a>
-					@endif
-					<form method="POST" action="{{ route('logout') }}">
-                            @csrf
-							<a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                this.closest('form').submit();" class="navbar-brand hover:text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16" style="display:inline;">
-  <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
-  <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-</svg> خروج از حساب </a>
-						
-                        </form>
-                        
-                    @else
-                        <a href="{{ route('login') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
-  <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-</svg> ورود به حساب </a>
+	<div class="container-fluid">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
-  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z"/>
-</svg>
-   ثبت نام</a>
-                        @endif
-                    @endauth
-					
-                </div>
-            @endif
-		</li>
-		
+		@if (Route::has('login'))
+
+		<a href="{{ route('IndexTravel') }}" class="navbar-brand hover:text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image-alt" viewBox="0 0 16 16" style="display:inline;">
+				<path d="M7 2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0zm4.225 4.053a.5.5 0 0 0-.577.093l-3.71 4.71-2.66-2.772a.5.5 0 0 0-.63.062L.002 13v2a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4.5l-4.777-3.947z" />
+			</svg> سفرها</a>
+
+		@auth
+		@if(Auth::user()->roles->where("role",'Admin')->count() == "1")
+		<a href="{{ route('CreateTravel') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16" style="display:inline;">
+				<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+			</svg> افزودن سفر </a>
+		@endif
+		<form method="POST" action="{{ route('logout') }}">
+			@csrf
+			<a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                this.closest('form').submit();" class="navbar-brand hover:text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16" style="display:inline;">
+					<path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+					<path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+				</svg> خروج از حساب </a>
+
+		</form>
+
+		@else
+		<a href="{{ route('login') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+				<path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+			</svg> ورود به حساب </a>
+
+		@if (Route::has('register'))
+		<a href="{{ route('register') }}" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
+				<path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z" />
+			</svg>
+			ثبت نام</a>
+		@endif
+		@endauth
+
+	</div>
+	@endif
+	</li>
+
 	</ul>
-  </div>
+	</div>
 </nav>
 <x-app-layout>
 
-	    <div class="py-12" style="font-size:18px;">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200" dir=rtl>
-				 <p style="color:green"> سلام ادمین </p>
+	<div class="py-12" style="font-size:18px;">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+				<div class="p-6 bg-white border-b border-gray-200" dir=rtl>
+					@if($photo_url == "/storage/files/null")
+					<img src="admin.jpg" class="img-fluid img-circle" alt="Cover Image Avialable">
+
+					@else
+					<img src={{$photo_url}} class="img-fluid img-circle" alt="Cover Image Avialable">
+					@endif
+					<p style="color:green"> سلام ادمین </p>
 					<p>نام :
-					{{$user->name}}
-						</p>
-						<p> ایمیل :
+						{{$user->name}}
+					</p>
+					<p> ایمیل :
 						{{$user->email}}
-						</p>
-						<p> شماره تماس :
+					</p>
+					<p> شماره تماس :
 						{{$user->phone}}
-						</p>
-						<p> سال تولد :
+					</p>
+					<p> سال تولد :
 						{{$user->birthdate}}
-						</p>
-						<p> حساب در تاریخ 
+					</p>
+					<p> حساب در تاریخ
 						{{$user->created_at}}
-						 ایجاد شده است </p>
-                </div>
+						ایجاد شده است
+					</p>
+				</div>
 				@if($user->travels->count() != 0)
 				<div class="p-6 bg-white border-b border-gray-200" dir=rtl>
 					<p>سفرهای شما :</p>
 					@foreach ($user->travels as $travel)
 					<a href="{{route('ShowTravel',[$travel])}}"> مقصد: {{$travel->destination}}</a>
-								<p>زمان سفر: {{$travel->traveltime}}</p>
-								<p> شروع ثبت نام: {{$travel->registerationstart}}</p>
-								<p> پایان ثبت نام : {{$travel->registerationend}}</p>
-								<p> توضیحات سفر : {{$travel->description}}</p>
-								@if($travel->cancel == 1)
-									<p style="color:red; font-size:18px;"> این سفر کنسل شده است </p>
-								@endif
-								<hr style="height:2px;border-width:0;color:gray;background-color:gray">
+					<p>زمان سفر: {{$travel->traveltime}}</p>
+					<p> شروع ثبت نام: {{$travel->registerationstart}}</p>
+					<p> پایان ثبت نام : {{$travel->registerationend}}</p>
+					<p> توضیحات سفر : {{$travel->description}}</p>
+					@if($travel->cancel == 1)
+					<p style="color:red; font-size:18px;"> این سفر کنسل شده است </p>
+					@endif
+					<hr style="height:2px;border-width:0;color:gray;background-color:gray">
 					@endforeach
-                </div>
+				</div>
 				@endif
-				
+
 				@if($nonactive_leaders->count() != 0)
 				<div class="p-6 bg-white border-b border-gray-200" dir=rtl>
 					<p>لیدرهای ثبت نام کرده :</p>
 					@foreach ($nonactive_leaders as $leader)
 					<p>نام :{{$leader->user->name}}</p>
-						@if($leader->active == 1)
-							<a href="{{route('unactiveleader',[$leader])}}"><button id="submitbutton">غیر فعال کردن</button></a>
-						@else
-							<a href="{{route('activeleader',[$leader])}}"><button id="submitbutton">فعال کردن</button></a>
-						@endif
+					@if($leader->active == 1)
+					<a href="{{route('unactiveleader',[$leader])}}"><button id="submitbutton">غیر فعال کردن</button></a>
+					@else
+					<a href="{{route('activeleader',[$leader])}}"><button id="submitbutton">فعال کردن</button></a>
+					@endif
 					@endforeach
-                </div>
+				</div>
 				@endif
-            </div>
-        </div>
-    </div>
-	</x-app-layout>
+			</div>
+		</div>
+	</div>
+</x-app-layout>
 @endsection
