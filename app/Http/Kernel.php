489744538de9ba30e8253800316e5a1905324524
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-10 00:36:53 +0430

    delete leader from middleware , we have adminOrLeader

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 		'admin' =>  \App\Http\Middleware\EnsureItIsAdmin::class, 
-		'leader' => \App\Http\Middleware\EnsureItIsLeader::class,
 		'adminorleader' =>\App\Http\Middleware\CheckBeAdminOrLeader::class,
     ];
 }
