ff09c87b00a9c4a7f8577427433231827a934d4c
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-09 20:08:50 +0430

    use verta

--- config/app.php
+++ config/app.php
@@ -141,6 +141,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Hekmatinasser\Verta\Laravel\VertaServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -163,7 +164,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        
         /*
          * Package Service Providers...
          */
@@ -191,7 +192,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'Verta' => Hekmatinasser\Verta\Verta::class
     ])->toArray(),
 
 ];
