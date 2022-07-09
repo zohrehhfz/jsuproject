e1613ebf56a10f84da35b4e330c5ca51fe15dbe7
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-09 18:56:32 +0430

    clean code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -13,36 +14,33 @@ use App\Models\Role;
 class UserController extends Controller
 {
 	public function __construct()
-    {
+	{
 		$this->middleware('auth');
-       // $this->middleware('admin')->except(['show','index','AddTravelForUser']);
+		// $this->middleware('admin')->except(['show','index','AddTravelForUser']);
 		// $this->middleware('leader')->except(['show','index','AddTravelForUser']);
-    }
-	
-    public function redirectTo()
-    {
+	}
+
+	public function redirectTo()
+	{
 		$user = Auth::user();
 		$user->load('travels');
-		$url = Storage::url('public/files/'.$user->photoname);
-        if (Auth::user()->roles->where('role','Admin')->count() == 1) 
-		{
-			$nonactive_leaders = Role::all()->where('role','=','leader');
+		$url = Storage::url('public/files/' . $user->photoname);
+		if (Auth::user()->roles->where('role', 'Admin')->count() == 1) {
+			$nonactive_leaders = Role::all()->where('role', '=', 'leader');
 			$nonactive_leaders->load('user');
-            return view('/panels/admin',['user'=>$user , 'nonactive_leaders'=>$nonactive_leaders,'photo_url'=>$url]);
-        }
-		else
-		{
-        return view('dashboard',['user'=>$user,'photo_url'=>$url]);
+			return view('/panels/admin', ['user' => $user, 'nonactive_leaders' => $nonactive_leaders, 'photo_url' => $url]);
+		} else {
+			return view('dashboard', ['user' => $user, 'photo_url' => $url]);
 		}
-    }
+	}
 	public function CancleTrvaelForUser(Travel $travel)
-    {
+	{
 		$user = Auth::user();
 		$r = $user->travels()->where('travel_id', $travel->id)->firstOrFail()->pivot->role;
-		if(($r == "user") || ($r == "leader"))
+		if (($r == "user") || ($r == "leader"))
 			$user->travels()->detach($travel);
 		$message = "3";
 		$number = $travel->users()->count();
-		return view('travels.show',['travel'=>$travel , 'message'=>$message ,'number'=>$number]);
-    }
+		return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number]);
+	}
 }
