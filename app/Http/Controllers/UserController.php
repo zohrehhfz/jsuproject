ed861e8a082abb700312e6b107901340de42e56c
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-10 12:47:53 +0430

    move AddTravelForUser from Travelcontroller to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -61,4 +61,48 @@ class UserController extends Controller
 			return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number, 'leader_name' => $leader_name, 'role' => 0]);
 		}
 	}
+
+	public function AddTravelForUser(Travel $travel)
+	{
+		$user = Auth::user();
+		$user_with_travels = $user->travels;
+		$count = $user_with_travels->where('id', $travel->id)->count();
+		if ($count == 0) 
+		{
+			$user->travels()->attach($travel, ['role' => "user"]);
+			$message = "1";
+		} else {
+			$message = "5";
+		}
+
+		$number = $travel->users()->count();
+		$r = $travel->users;
+		$leader_name = "empty";
+		for ($i = 0; $i < $r->count(); $i++) {
+			if ($r[$i]->pivot->role == "leader") {
+				$leader_name = $r[$i]->name;
+			}
+		}
+		$r = $travel->users;
+		$leader_name = "empty";
+		for ($i = 0; $i < $r->count(); $i++) {
+			if ($r[$i]->pivot->role == "leader") {
+				$leader_name = $r[$i]->name;
+			}
+		}
+		if (Auth::user()) {
+			$user = Auth::user();
+			$admin = $user->roles->where('role', 'Admin')->count();
+
+			$leader = $user->roles->where('role', 'leader')->count();
+			if (($admin == 1) || ($leader == 1)) {
+				$travel->users;
+				return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number, 'leader_name' => $leader_name, 'role' => 1]);
+			} else {
+				return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number, 'leader_name' => $leader_name, 'role' => 0]);
+			}
+		} else {
+			return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number, 'leader_name' => $leader_name, 'role' => 0]);
+		}
+	}
 }
