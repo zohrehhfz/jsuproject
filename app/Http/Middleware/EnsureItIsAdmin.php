62df962615eff9d93b0e01a4817c2fc97b6aed95
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-10 10:25:02 +0430

    create EnsureItIsAdmin middleware

---
+++ app/Http/Middleware/EnsureItIsAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Role;
+use App\Models\User;
+
+class EnsureItIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+		$u = auth()->user();
+		$a = $u->roles->where('role','Admin')->count();
+		
+		if($a != 1)
+		{
+			abort(403, 'Access denied');
+		}		
+        return $next($request);
+    }
+}
