90e908f2bccf7cff4e0c9104306b01da17a2104f
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-10 10:24:30 +0430

    change retrun show page with leadername , role user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,11 +36,29 @@ class UserController extends Controller
 	public function CancleTrvaelForUser(Travel $travel)
 	{
 		$user = Auth::user();
-		$r = $user->travels()->where('travel_id', $travel->id)->firstOrFail()->pivot->role;
-		if (($r == "user") || ($r == "leader"))
+		$r = $user->travels()->where('travel_id', $travel->id)->get();
+		if ($r != null)
 			$user->travels()->detach($travel);
 		$message = "3";
 		$number = $travel->users()->count();
-		return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number]);
+
+		$r = $travel->users;
+		$leader_name = "empty";
+		for ($i = 0; $i < $r->count(); $i++) {
+			if ($r[$i]->pivot->role == "leader") {
+				$leader_name = $r[$i]->name;
+			}
+		}
+
+
+		$admin = $user->roles->where('role', 'Admin')->count();
+
+		$leader = $user->roles->where('role', 'leader')->count();
+		if (($admin == 1) || ($leader == 1)) {
+			$travel->users;
+			return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number, 'leader_name' => $leader_name, 'role' => 1]);
+		} else {
+			return view('travels.show', ['travel' => $travel, 'message' => $message, 'number' => $number, 'leader_name' => $leader_name, 'role' => 0]);
+		}
 	}
 }
