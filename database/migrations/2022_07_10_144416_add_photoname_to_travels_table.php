f7152118e7dff0679e7a9127b2dbd22a638bec93
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-16 20:10:51 +0430

    add photo column to travels Table

---
+++ database/migrations/2022_07_10_144416_add_photoname_to_travels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->string("photoname")->after("cancel")->nullable();
+			$table->string("orginalphotoname")->after("photoname")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->dropColumn('photoname');
+			$table->dropColumn('orginalphotoname');
+        });
+    }
+};
