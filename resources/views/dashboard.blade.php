96d21518ddfa4d93e419550f8c3d19ed9540f56e
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-18 18:55:10 +0430

    Add button for update registery info

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -59,10 +59,12 @@
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
 							<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
 						</svg>
-						<div>
-							برای اینکه حساب شما به عنوان لیدر فعال شود مدرک خود را آپلود کنید.
+						برای اینکه حساب شما به عنوان لیدر فعال شود مدرک خود را آپلود کنید.
+						<div style="margin:auto">
+							<button id="submitbutton" style="margin-left: -30vw;"><a style="color:white; text-decoration: none; "> به روز رسانی حساب</a></button>
 						</div>
 					</div>
+
 					@endif
 					@if($photo_url == "/storage/files/null")
 					<img src="user.gif" class="img-fluid img-circle" alt="profile photo Not Set">
