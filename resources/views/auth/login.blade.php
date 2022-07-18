c842be633f9a7c6a48fb1fce4b03931d1d67f59e
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-18 12:53:53 +0430

    change text to persian

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,39 +49,41 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('ایمیل')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('رمزعبور')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="current-password" />
+                                required autocomplete="current-password" style="margin-left: 10px;"/>
             </div>
 
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('یادآوری') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <x-button class="ml-3" id="submitbutton">
+                    {{ __('وارد شدن') }}
+                </x-button>
+                <br>
+                <br>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('رمز عبور خود را فراموش کرده‌اید؟') }}
                     </a>
                 @endif
 
-                <x-button class="ml-3" id="submitbutton">
-                    {{ __('Log in') }}
-                </x-button>
             </div>
         </form>
             
