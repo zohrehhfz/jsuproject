2a99f6170357dc8047dd019a5c5d28853808a08f
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-19 16:09:29 +0430

    change view of page

--- resources/views/panels/upadatepanel.blade.php
+++ resources/views/panels/upadatepanel.blade.php
@@ -1,69 +1,61 @@
 @extends('layouts.my_layout')
-@section('title', 'ایجاد حساب ')
+@section('title', 'به روزرسانی حساب ')
 @section('content')
 <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-<form method="POST" action="" dir="rtl" enctype="multipart/form-data">
+<form method="POST" action="{{route('UpdateUserLeaderInfo')}}" dir="rtl" enctype="multipart/form-data">
     @csrf
     <div class="container">
         <div class="row divupload">
             <div class=" col-6">
-                <div> آپلود عکس شما : </div>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+  <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg> آپلود عکس شما : </p>
                 <div> <input type="file" name="photo" id="photo"> </div>
                 <br>
-                <div> آپلود مدرک شما : </div>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+  <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg> آپلود مدرک شما : </p>
                 <div> <input type="file" name="certificate"> </div>
                 <br>
             </div>
             <!-- Name -->
             <div class=" col-6">
                 <div>
-                    <x-label for="name" :value="__('نام')" />
+                    <p style="margin: auto; margin-left:10vw"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg>نام</p>
 
                     <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{$user->name}}" required autofocus />
                 </div>
 
                 <!-- Email Address -->
                 <div class="mt-4">
-                    <x-label for="email" :value="__('ایمیل')" />
+                    <p style="margin: auto; margin-left:9vw"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+</svg>ایمیل </p>
 
-                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{$user->email}}" required style="margin-left: 15px;" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{$user->email}}" required style="margin-left: 5px;" />
                 </div>
 
                 <!-- phone -->
                 <div class="mt-4">
-                    <x-label for="phone" :value="__('شماره تلفن')" />
+                    <p style="margin: auto; margin-left:7vw"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>شماره تلفن</p>
 
-                    <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" value="{{$user->phone}}" required style="margin-left: 55px;" />
+                    <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" value="{{$user->phone}}" required style="margin-left: 8px;" />
                 </div>
                 <!-- year of birth -->
                 <div class="mt-4">
-                    <x-label for="birthdate" :value="__('سال تولد')" />
-
-                    <x-input id="birthdate" class="block mt-1 w-full" type="text" name="birthdate" value="{{$user->birthdate}}" required style="margin-left: 40px;" />
+                <p style="margin: auto; margin-left:8vw"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-day-fill" viewBox="0 0 16 16">
+  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zm9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5zm-2.24 4.855a.428.428 0 1 0 0-.855.426.426 0 0 0-.429.43c0 .238.192.425.43.425zm.337.563h-.672v4.105h.672V8.418zm-6.867 4.105v-2.3h2.261v-.61H4.684V7.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V9.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105z"/>
+</svg>سال تولد</p>
+                    <x-input id="birthdate" class="block mt-1 w-full" type="text" name="birthdate" value="{{$user->birthdate}}" required style="margin-left: 10px;" />
                 </div>
 
-                <div class="mt-4">
-
-                    <input type="radio" id="user" name="role" value="user">
-                      <label for="html">کاربر</label>
-                      <input type="radio" id="leader" name="role" value="leader">
-                      <label for="css">لیدر</label>
-                </div>
-
-                <!-- Password -->
-                <div class="mt-4">
-                    <x-label for="password" :value="__('رمز عبور')" />
-
-                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" style="margin-left: 40px;" />
-                </div>
-
-                <!-- Confirm Password -->
-                <div class="mt-4">
-                    <x-label for="password_confirmation" :value="__('تکرار رمز عبور')" />
-
-                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required style="margin-left: 70px;" />
-                </div>
             </div>
             <div class="flex items-center justify-end mt-4">
 
