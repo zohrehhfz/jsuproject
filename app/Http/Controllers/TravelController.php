9090b6fe2fedc935e781a9198fe78182143f316a
zohrehhfz <hfz.zohreh@gmail.com>
2022-07-31 01:11:15 +0430

    Write SetComment function

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use App\Models\Travel;
+use App\Models\Comment;
 use App\Models\User;
 
 class TravelController extends Controller
@@ -280,15 +281,21 @@ class TravelController extends Controller
 							if (($admin == 1) || ($leader == 1)) {
 								$travel->users;
 								$travel->comments;
-								return view('travels.show', ['travel' => $travel, 'message' => $message,
-								 'number' => $number, 'leader_name' => $leader_name, 'role' => 1,'photo_url' => $url]);
+								return view('travels.show', [
+									'travel' => $travel, 'message' => $message,
+									'number' => $number, 'leader_name' => $leader_name, 'role' => 1, 'photo_url' => $url
+								]);
 							} else {
-								return view('travels.show', ['travel' => $travel, 'message' => $message,
-								 'number' => $number, 'leader_name' => $leader_name, 'role' => 0,'photo_url' => $url]);
+								return view('travels.show', [
+									'travel' => $travel, 'message' => $message,
+									'number' => $number, 'leader_name' => $leader_name, 'role' => 0, 'photo_url' => $url
+								]);
 							}
 						} else {
-							return view('travels.show', ['travel' => $travel, 'message' => $message,
-							 'number' => $number, 'leader_name' => $leader_name, 'role' => 0,'photo_url' => $url]);
+							return view('travels.show', [
+								'travel' => $travel, 'message' => $message,
+								'number' => $number, 'leader_name' => $leader_name, 'role' => 0, 'photo_url' => $url
+							]);
 						}
 					} else {
 						return redirect()->back()->withErrors(['error' => 'تاریخ ها معتبر نیستند']);
@@ -357,5 +364,48 @@ class TravelController extends Controller
 		}
 
 		return redirect()->back()->withErrors(['error' => 'شما اجازه فعال کردن این سفر را ندارید']);
+	}
+	public function SetComment(Request $request)
+	{
+		$request->validate([
+			'travel' => 'required',
+			'message' => 'required',
+		]);
+		$user = Auth::user();
+		$travel_id = $request->travel;
+		Comment::Create([
+			"from" => $user->id,
+			"travel_id" => $travel_id,
+			"message" => $request->message,
+		]);
+		$travel = Travel::find($travel_id);
+		$message = "0";
+		$number = $travel->users()->count();
+		$r = $travel->users;
+		$leader_name = "empty";
+		for ($i = 0; $i < $r->count(); $i++) {
+			if ($r[$i]->pivot->role == "leader") {
+				$leader_name = $r[$i]->name;
+			}
+		}
+		$url = Storage::url('public/travels/' . $travel->photoname);
+
+		$admin = $user->roles->where('role', 'Admin')->count();
+
+		$leader = $user->roles->where('role', 'leader')->count();
+
+		if (($admin == 1) || ($leader == 1)) {
+			$travel->users;
+			$travel->comments;
+			return view('travels.show', [
+				'travel' => $travel, 'message' => $message,
+				'number' => $number, 'leader_name' => $leader_name, 'role' => 1, 'photo_url' => $url
+			]);
+		} else {
+			return view('travels.show', [
+				'travel' => $travel, 'message' => $message,
+				'number' => $number, 'leader_name' => $leader_name, 'role' => 0, 'photo_url' => $url
+			]);
+		}
 	}
 }
