5016b815d7bffa7d03e00bea135cae75902a5d03
zohrehhfz <hfz.zohreh@gmail.com>
2022-08-02 01:20:27 +0430

    Change Route for show chat Room

--- resources/views/travels/show.blade.php
+++ resources/views/travels/show.blade.php
@@ -129,7 +129,7 @@
 					$count_t = $user_with_travels->where('id', $travel->id)->count();
 					$admin = $user->roles->where('role', 'Admin')->count();
 					if (($count_t > 0) || ($admin > 0)) { ?>
-						<button id="submitbutton"><a href="{{route('ShowChat')}}" style="color:white; text-decoration: none;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-text-fill" viewBox="0 0 16 16">
+						<button id="submitbutton"><a href="{{route('ShowChat',[$travel])}}" style="color:white; text-decoration: none;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-text-fill" viewBox="0 0 16 16">
 									<path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z" />
 								</svg> صفحه چت </a></button>
 				<?php
