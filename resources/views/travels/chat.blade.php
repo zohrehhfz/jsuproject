544dcd23b0ca0c810eff1dcbdcf55431769ec0f6
zohrehhfz <hfz.zohreh@gmail.com>
2022-08-02 00:30:54 +0430

    Create Chat page

---
+++ resources/views/travels/chat.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.my_layout')
+@section('title', 'چت')
+@section('content')
+<x-app-layout>
+
+	<div class="py-12" style="font-size:18px;">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+				<div class="p-6 bg-white border-b border-gray-200" dir=rtl style="margin: auto;">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</x-app-layout>
+@endsection
