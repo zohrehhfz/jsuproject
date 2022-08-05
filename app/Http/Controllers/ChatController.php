2b9879eb23bd0b3374485dd823c4c82609ce4163
zohrehhfz <hfz.zohreh@gmail.com>
2022-08-05 23:41:02 +0430

    Add function SendMessage

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -9,13 +9,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use App\Models\Travel;
+use App\Models\Chat;
 use App\Models\Comment;
 use App\Models\User;
 use Verta;
 
 class ChatController extends Controller
 {
-    public function ShowChat(Travel $travel)
+	public function ShowChat(Travel $travel)
 	{
 		$user = Auth::user();
 		if ($user) {
@@ -24,8 +25,32 @@ class ChatController extends Controller
 			$admin = $user->roles->where('role', 'Admin')->count();
 			if (($count_t > 0) || ($admin > 0)) {
 				$chats = $travel->chats;
-				return view('travels.chat' , ["chats" => $chats , "user" => $user]);
+				return view('travels.chat', ["chats" => $chats, "user" => $user]);
 			}
+		}
+	}
+	public function SendMessage(Request $request)
+	{
+		$request->validate([
+			'travel' => 'required',
+			'message' => 'required',
+		]);
+		$user = Auth::user();
+		$travel_id = $request->travel;
+		Chat::Create([
+			"from" => $user->id,
+			"travel_id" => $travel_id,
+			"parent_id" => NULL,
+			"flag" => 0,
+			"message" => $request->message,
+		]);
+		$user_with_travels = $user->travels;
+		$count_t = $user_with_travels->where('id', $travel_id)->count();
+		$travel = Travel::find($travel_id);
+		$admin = $user->roles->where('role', 'Admin')->count();
+		if (($count_t > 0) || ($admin > 0)) {
+			$chats = $travel->chats;
+			return view('travels.chat', ["chats" => $chats, "user" => $user]);
 		}
 	}
 }
