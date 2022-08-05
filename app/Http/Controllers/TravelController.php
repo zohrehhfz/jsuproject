c6212682994f5b4b9b5fe789546ea7be72d61206
zohrehhfz <hfz.zohreh@gmail.com>
2022-08-05 18:48:08 +0430

    Remove Setcomment , ShowChat from this , move to wright Controller

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -366,60 +366,6 @@ class TravelController extends Controller
 
 		return redirect()->back()->withErrors(['error' => 'شما اجازه فعال کردن این سفر را ندارید']);
 	}
-	public function SetComment(Request $request)
-	{
-		$request->validate([
-			'travel' => 'required',
-			'message' => 'required',
-		]);
-		$user = Auth::user();
-		$travel_id = $request->travel;
-		Comment::Create([
-			"from" => $user->id,
-			"travel_id" => $travel_id,
-			"message" => $request->message,
-		]);
-		$travel = Travel::find($travel_id);
-		$message = "0";
-		$number = $travel->users()->count();
-		$r = $travel->users;
-		$leader_name = "empty";
-		for ($i = 0; $i < $r->count(); $i++) {
-			if ($r[$i]->pivot->role == "leader") {
-				$leader_name = $r[$i]->name;
-			}
-		}
-		$url = Storage::url('public/travels/' . $travel->photoname);
-
-		$admin = $user->roles->where('role', 'Admin')->count();
-
-		$leader = $user->roles->where('role', 'leader')->count();
-
-		if (($admin == 1) || ($leader == 1)) {
-			$travel->users;
-			$travel->comments;
-			return view('travels.show', [
-				'travel' => $travel, 'message' => $message,
-				'number' => $number, 'leader_name' => $leader_name, 'role' => 1, 'photo_url' => $url
-			]);
-		} else {
-			return view('travels.show', [
-				'travel' => $travel, 'message' => $message,
-				'number' => $number, 'leader_name' => $leader_name, 'role' => 0, 'photo_url' => $url
-			]);
-		}
-	}
-	public function ShowChat(Travel $travel)
-	{
-		$user = Auth::user();
-		if ($user) {
-			$user_with_travels = $user->travels;
-			$count_t = $user_with_travels->where('id', $travel->id)->count();
-			$admin = $user->roles->where('role', 'Admin')->count();
-			if (($count_t > 0) || ($admin > 0)) {
-				$chats = $travel->chats;
-				return view('travels.chat' , ["chats" => $chats , "user" => $user]);
-			}
-		}
-	}
+	
+	
 }
